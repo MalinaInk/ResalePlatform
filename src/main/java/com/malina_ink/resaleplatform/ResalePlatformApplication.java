32c090cb243f8dbfb5e0589b12d9723b0184f7d5
mo <nataleshen.ko@yandex.ru>
2023-06-22 15:29:38 +0300

    initial commit

---
+++ src/main/java/com/malina_ink/resaleplatform/ResalePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.malina_ink.resaleplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResalePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResalePlatformApplication.class, args);
+    }
+
+}
