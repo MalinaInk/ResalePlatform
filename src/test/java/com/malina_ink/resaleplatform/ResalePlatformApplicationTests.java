9b4d6e65114037f2f76de2863b300c903702c20e
mo <nataleshen.ko@yandex.ru>
2023-06-23 16:07:15 +0300

    add property commit

---
+++ src/test/java/com/malina_ink/resaleplatform/ResalePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malina_ink.resaleplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResalePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
