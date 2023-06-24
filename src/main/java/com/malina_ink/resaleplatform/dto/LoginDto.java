3520814de1a343dd537ee4d95b65d7223994ebe8
Daria <m-alberts@yandex.ru>
2023-06-24 09:46:56 +0300

    Add DTO commit

--- src/main/java/com/malina_ink/resaleplatform/dto/LoginDto.java
+++ src/main/java/com/malina_ink/resaleplatform/dto/LoginDto.java
@@ -40,5 +40,4 @@ public class LoginDto {
                 ", password='" + password + '\'' +
                 '}';
     }
-
 }
--- src/main/java/com/malina_ink/resaleplatform/enums/Role.java
+++ src/main/java/com/malina_ink/resaleplatform/enums/Role.java
@@ -2,5 +2,4 @@ package com.malina_ink.resaleplatform.enums;
 
 public enum Role {
     USER, ADMIN
-
 }
