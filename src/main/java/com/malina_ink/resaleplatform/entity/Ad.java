4895b018346f9b2af9b0634b9be9b1aee2a5dc72
mo <nataleshen.ko@yandex.ru>
2023-06-27 19:31:16 +0300

    correct entity

--- src/main/java/com/malina_ink/resaleplatform/entity/Ad.java
+++ src/main/java/com/malina_ink/resaleplatform/entity/Ad.java
@@ -1,22 +1,30 @@
 package com.malina_ink.resaleplatform.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
-public class Ads {
+public class Ad {
+
+    private String id;
+
     private int author;
     private String image;
-    private String id;
     private int price;
     private String title;
     private String description;
     private String AdImage;
+    private List<Comment> comments = new ArrayList<>();
 
-    int count = 0;
-    private static List<Ads> ads = new ArrayList<>();
 
 }
